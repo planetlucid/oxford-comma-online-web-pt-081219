6d2e26a933923fb6d79f5c4e98f77f1cf455b2b6
Ryan S <planetlucid@gmail.com>
2019-08-20 22:28:18 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,26 @@
+require "pry"
+
 def oxford_comma(array)
+  if array.length == 1
+     array.join ("")
+    
+  elsif array.length == 2
+     "#{array[0]} and #{array[1]}"
+
+  elsif array.length == 3
+     "#{array[0]}, #{array[1]}, and #{array[2]}"
+  
+  else 
+   #array.insert(-2, "and")
+   array[-1].insert(0, "and ")
+   array.join(", ")
+  
+  #binding.pry 
+ 
+  
+  
+  
+  end
 
 end
+
