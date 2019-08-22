defa23845a23dbe0fa6d6bce9dba85dca1689f6b
Ryan S <planetlucid@gmail.com>
2019-08-22 12:08:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,8 @@ def oxford_comma(array)
      array.join ("")
     
   elsif array.length == 2
-     "#{array[0]} and #{array[1]}"
+     array.join(" and ")
+     #"#{array[0]} and #{array[1]}"
 
   elsif array.length == 3
      "#{array[0]}, #{array[1]}, and #{array[2]}"
